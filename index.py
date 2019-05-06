c0296fb8147fbaec29610ec3958541224205c73b
Dominic Humber <dhumber.buisness@gmail.com>
2019-05-06 18:25:40 -0400

    Create index.py
    Created the layout for pygame (Displays the screen). Added a class to controll all the rays that we will be creating.

---
+++ index.py
@@ -0,0 +1,27 @@
+import pygame as pg
+import math
+
+class Rays:
+    def __init__(self):
+        pass
+
+def main():
+    screen = pg.display.set_mode((640,480))
+    pg.display.set_caption("Refracting Telescope")
+
+    clock = pg.time.Clock()
+    
+    while True:
+        clock.tick(60)
+        background = pg.Surface(screen.get_size())
+        background = background.convert()
+        background.fill((38,1,1))
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:   break
+        
+        screen.blit(background, (0,0))
+        pg.display.flip()
+
+main()
+pg.quit()
