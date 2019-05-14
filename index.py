1361af8ce3a1cdf950279bc8f40eba4643dceb33
gerauschlos <50221495+gerauschlos@users.noreply.github.com>
2019-05-14 09:30:52 -0400

    Lens Update
    Added the lens to the code!

--- index.py
+++ index.py
@@ -2,14 +2,18 @@ import pygame as pg
 import math
 
 class Rays:
-    def __init__(self):
-        pass
+    def __init__(self,location=[],destination=[]):
+        self.location = location
+        self.destination = destination
 
 def main():
     screen = pg.display.set_mode((640,480))
     pg.display.set_caption("Refracting Telescope")
 
+    height_object = 10
+    distance_object = 5
     clock = pg.time.Clock()
+
     
     while True:
         clock.tick(60)
@@ -20,10 +24,13 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT:   break
 
-        pg.draw.line(background,(0,0,0),[100,240],[540, 240],3)    
-        pg.draw.arc(background,(255,0,0),[540,150,50,200],     .05,   -.05, 2)    
+        pg.draw.line(background,(0,0,0),[100,240],[540, 240],3)
+        pg.draw.ellipse(background,(255,255,0),[350,90+70,25,150],0)
+        pg.draw.ellipse(background,(255,255,0),[450,140+50,25,100],0)    
         screen.blit(background, (0,0))
         pg.display.flip()
+
+
 
 main()
 pg.quit()
