bebf02b5535ee8f6979b629467d9cddbb4f94b7e
mattheglass <50587063+mattheglass@users.noreply.github.com>
2019-05-15 11:31:01 -0400

    Test Rays

--- index.py
+++ index.py
@@ -14,6 +14,9 @@ def main():
     distance_object = 5
     clock = pg.time.Clock()
 
+    #tree = pygame.image.load("tree.jpg")
+    #tree = tree.convert()
+
     
     while True:
         clock.tick(60)
@@ -24,9 +27,13 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT:   break
 
+        pg.draw.line(background,(0,0,0),[100,165],[360, 165],3)
+        pg.draw.line(background,(0,0,0),[360,165],[440, 270],3)
         pg.draw.line(background,(0,0,0),[100,240],[540, 240],3)
         pg.draw.ellipse(background,(255,255,0),[350,90+70,25,150],0)
-        pg.draw.ellipse(background,(255,255,0),[450,140+50,25,100],0)    
+        pg.draw.ellipse(background,(255,255,0),[450,140+50,25,100],0)
+        
+        #screen.blit(tree, (290, 190))
         screen.blit(background, (0,0))
         pg.display.flip()
 
@@ -34,4 +41,3 @@ def main():
 
 main()
 pg.quit()
-
