bfee0f1de76b5de8d9956bddbdde808ef02f6fc4
gerauschlos <50221495+gerauschlos@users.noreply.github.com>
2019-05-16 10:16:29 -0400

    Changed Background
    Not completed yet but the new background image was added.

--- index.py
+++ index.py
@@ -5,14 +5,20 @@ class Rays:
     def __init__(self,destination=[]):
         self.destination = destination
 
+    def run():
+        pass
+    
+    def change_course():
+        pass
+
 def main():
-    screen = pg.display.set_mode((640,480))
+    screen = pg.display.set_mode((1000,900))
     pg.display.set_caption("Refracting Telescope")
 
     height_object = 10
     distance_object = 5
     clock = pg.time.Clock()
-
+    background = pg.image.load("epic.jpg")
     tree = pg.image.load("tree.jpg")
     tree = tree.convert()
 
@@ -21,9 +27,10 @@ def main():
     
     while True:
         clock.tick(60)
-        background = pg.Surface(screen.get_size())
+        """"background = pg.Surface(screen.get_size())
         background = background.convert()
-        background.fill((136, 204, 226))
+        background.fill((136, 204, 226))"""
+        pg.transform.smoothscale(background, (1000,900))
 
         for event in pg.event.get():
             if event.type == pg.QUIT:   break
@@ -31,8 +38,8 @@ def main():
         pg.draw.line(background,(0,0,0),[100,165],[360, 165],3)
         pg.draw.line(background,(0,0,0),[360,165],[440, 270],3)
         pg.draw.line(background,(0,0,0),[100,240],[540, 240],3)
-        pg.draw.ellipse(background,(255,255,0),[350,90+70,25,150],0)
-        pg.draw.ellipse(background,(255,255,0),[450,140+50,25,100],0)
+        pg.draw.ellipse(background,(255, 255, 255),[350,90+70,25,150],0)
+        pg.draw.ellipse(background,(255, 255, 255),[450,140+50,25,100],0)
         
         
         screen.blit(background, (0,0))
