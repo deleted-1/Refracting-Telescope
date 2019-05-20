171f125739e21ba8b67477328a32c38e3e929334
Dominic Humber <dhumber.buisness@gmail.com>
2019-05-20 00:09:05 -0400

    More features
    It is more automated fixed the recursion problem.

--- index.py
+++ index.py
@@ -1,19 +1,7 @@
 import pygame as pg
 import math
 
-"""
-I am too tired to explain
-what this class does
-but read through it and try to revert
-it back to how it originally was if
-you mess around with it
-
-
-But to make it short all it does is draw that second
-line you see when you run the program
-"""
 class Rays:
-    rays = []
     
     def __init__(self,location,destination,destination1=None,destination2=None):
         self.location = location
@@ -25,18 +13,23 @@ class Rays:
         self.y = location[1]
 
     def change_course(self,background):
-        new_course = Rays(self.destination,self.destination1,self.destination2)
-        Rays.rays.append(new_course)
-        for ray in Rays.rays:
-            ray.run(background)
-    
+        self.location = self.destination
+        self.destination = self.destination1
+        self.destination1 = self.destination2
+        self.destination2 = None
+        if self.destination == None: return
+        self.movement = [(self.destination[0]-self.location[0])/60,(self.destination[1]-self.location[1])/60]
+        self.x = self.location[0]
+        self.y = self.location[1]
+
     def run(self,background):
-        pg.draw.line(background,(0,0,0),self.location,[self.x,self.y],3)
-        if self.x < self.destination[0]: 
+        pg.draw.line(background,(255,255,0),self.location,[self.x,self.y],3)
+        if self.x <= self.destination[0]: 
             self.x += self.movement[0]
             self.y += self.movement[1]
         else:   
-            if self.destination1 != None:  self.change_course(background)
+            if self.destination1 != None: 
+                self.change_course(background)
 
 
 def main():
@@ -49,24 +42,27 @@ def main():
     clock = pg.time.Clock()
     tree = pg.image.load("tree.png")
     tree = pg.transform.scale(tree, [height_object,height_object])
-    tree = tree.convert()
-    center_curvature1 = [512, 450]
-    center_curvature2 = [712, 450]
-    focal_objective1 = [500-75,450]
-    focal_objective2 = [500+150,450]
-    focal_ocular1 = [700-50,450] 
-    focal_ocular2 = [700+75,450]
-
-    ray1 = Rays([distance_object+height_object/2,450-height_object],[510,450-height_object],focal_objective2)
-    #ray2 = Rays([0, 350],[500,232])
+    tree = tree.convert()  
+    height_objective = 200
+    height_ocular = 100 
+    distance_objective  =500
+    distance_ocular = 700
+    center_curvature1 = [distance_objective+12, 450]
+    center_curvature2 = [distance_ocular+12, 450]
+    focal_objective1 = [distance_objective-int(height_objective/2),450]
+    focal_objective2 = [distance_objective+int(height_objective/2 + 25),450]
+    focal_ocular1 = [distance_ocular-int(height_ocular/2),450] 
+    focal_ocular2 = [distance_ocular+int(height_ocular/2+25),450]  
+    destination1 = [center_curvature2[0],focal_objective2[1]+(-focal_objective2[0]+center_curvature2[0]-17)]
+    ray1 = Rays( [distance_object+int(height_object/2),450-height_object] , [center_curvature1[0],450-height_object] , destination1 , focal_ocular2)
+    background = pg.Surface(screen.get_size())
+    background = background.convert()
+    background.fill((210 , 180 , 140))   
     
     
     
     while True:
-        clock.tick(60)
-        background = pg.Surface(screen.get_size())
-        background = background.convert()
-        background.fill((136, 204, 226))        
+        clock.tick(60)     
 
         for event in pg.event.get():
             if event.type == pg.QUIT:   break
@@ -78,8 +74,8 @@ def main():
         ocular_distance = []
         ocular_height = []
 
-        objective = pg.draw.ellipse(background,(255,255,0),[500,300+75,25,150],0)
-        ocular = pg.draw.ellipse(background,(255,255,0),[700,350+50,25,100],0)
+        objective = pg.draw.ellipse(background,(135, 206, 235),[distance_objective,450-int(height_objective*.5),25,height_objective],0)
+        ocular = pg.draw.ellipse(background,(135, 206, 235),[distance_ocular,450-int(height_ocular*.5),25,height_ocular],0)
         pg.draw.circle(background,(0,0,0),center_curvature1,5)
         pg.draw.circle(background,(0,0,0),center_curvature2,5)
         pg.draw.circle(background,(0,0,0),focal_objective1,5)
@@ -95,5 +91,4 @@ def main():
         pg.display.flip()
 
 main()
-
 pg.quit()
