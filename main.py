81299f4f64cb30c47d10235b9a2cd145dba8522b
Dominic Humber <dhumber.buisness@gmail.com>
2019-05-21 00:27:27 -0400

    Shortened Code

--- main.py
+++ main.py
@@ -32,7 +32,11 @@ class Rays:
         else:   
             if self.destination1 != None:   self.change_course(background)
 
-def mainh(height_object=70,distance_object=140,height_objective=300,height_ocular=150,distance_objective=400,distance_ocular=600):
+def draw_circles(background,coordinates):
+    for i in range(len(coordinates)):
+        pg.draw.circle(background,(0,0,0),coordinates[i],5) 
+
+def main(height_object=70,distance_object=140,height_objective=300,height_ocular=150,distance_objective=400,distance_ocular=600):
     screen = pg.display.set_mode((1000,900), pg.RESIZABLE)
 
     pg.display.set_caption("Refracting Telescope")
@@ -78,24 +82,17 @@ def mainh(height_object=70,distance_object=140,height_objective=300,height_ocula
     background.fill((210 , 180 , 140))   
     
     pg.draw.line(background,(0,0,0),[100,450],[900, 450],3)# Boundary/Plane
-    
+    image_formed = False
+
+    objective = pg.draw.ellipse(background,(135, 206, 235),[distance_objective,450-int(height_objective*.5),25,height_objective],0)
+    ocular = pg.draw.ellipse(background,(135, 206, 235),[distance_ocular,450-int(height_ocular*.5),25,height_ocular],0)
+    draw_circles(background,[center_curvature1,center_curvature2,focal_objective1,focal_objective2,focal_ocular1,focal_ocular2])
+
     while True:
         clock.tick(60)     
 
         for event in pg.event.get():
             if event.type == pg.QUIT:   break
-
-        image_formed = False
-
-        objective = pg.draw.ellipse(background,(135, 206, 235),[distance_objective,450-int(height_objective*.5),25,height_objective],0)
-        ocular = pg.draw.ellipse(background,(135, 206, 235),[distance_ocular,450-int(height_ocular*.5),25,height_ocular],0)
-        pg.draw.circle(background,(0,0,0),center_curvature1,5)
-        pg.draw.circle(background,(0,0,0),center_curvature2,5)
-        pg.draw.circle(background,(0,0,0),focal_objective1,5)
-        pg.draw.circle(background,(0,0,0),focal_objective2,5)
-        pg.draw.circle(background,(0,0,0),focal_ocular1,5)
-        pg.draw.circle(background,(0,0,0),focal_ocular2,5)
-
 
         ray1.run(background)
         ray2.run(background)
@@ -103,34 +100,17 @@ def mainh(height_object=70,distance_object=140,height_objective=300,height_ocula
 
         if ray1.x >= float(final_destination[0]) and ray2.x >= float(final_destination[0]) and ray3.x >= float(final_destination[0]): break
 
-        if ray1.y <= ray2.y: 
-            image = pg.image.load("tree.png")
-            intersection = [ray1.x,ray2.y]
-            pg.transform.rotate(image,180)
-            screen.blit(image, intersection)
-
-
         screen.blit(background, (0,0))
         screen.blit(tree, (distance_object, 450-height_object))
-
+        if ray1.y <= ray2.y and image_formed == False and ray1.y>450 and ray1.x <= ray2.x: 
+            intersection = [int(ray1.x),int(ray2.y)]
+            image = pg.image.load("tree.png")
+            image = pg.transform.rotate(image,360)
+            image = pg.transform.scale(tree, [int(ray1.y-450),int(ray1.y-450)])
+            image.convert()
+            screen.blit(image, intersection)
+            image_formed = True
+        elif image_formed:  screen.blit(image, intersection)
         pg.display.flip()
-
-"""
-from tkinter import *
-
-root = Tk()
-root.title("Refracting Telescope")
-root.geometry("640x640+0+0")
-
-heading = Label(root, text="Refracting Telescope Simulating", font=("arial",40,"bold"), fg="steelblue").pack()
-
-name = StringVar()
-entry_box = Entry(root, textvariable=name,width=25,bg="lightgreen").place(x=300,y=200)
-
-def Submit():
-    print(str(name.get()))
-
-work = Button(root, text="Work",width=30,height=5,bg="lightblue",command=Submit).place(x=250,y=300)
-"""
 
 pg.quit()
--- window.py
+++ window.py
@@ -1,13 +1,13 @@
 from tkinter import *
-from main import *
+from main import main
 
 def set_parameters():
 
     def show_diagram():
         try:
-            distance_objective
-            distance_ocular
-            main(int(focal1.get()), int(focal2.get()), int(object_distance.get()), int(object_height.get()))
+            distance_objective = ''
+            distance_ocular = ''
+            main()
         except ZeroDivisionError:
             set_parameters()
     
@@ -25,12 +25,17 @@ def set_parameters():
     distance_object=Entry(root)
     height_objective=Entry(root)
     height_ocular=Entry(root)
+    len_distance=Entry(root,text="hello world!")
     
 
-    height_object.place()
+    height_object.place(x=225,y=20)
+    distance_object.place(x=340, y=45)
+    height_objective.place(x=275,y=70)
+    height_ocular.place(x=250,y=95)
+    len_distance.place(x=300,y=120)
  
-    Button(root, text='Quit', command=root.quit).grid(row=4, column=0, sticky=W, pady=4)
-    Button(root, text='Enter', command=show_diagram).grid(row=4, column=500, sticky=W, pady=4)
+    Button(root, text='Quit', width = 5,command=root.quit).place(x=20,y=145)
+    Button(root, text='Submit',width=5, command=show_diagram).place(x=100,y=145)
 
     mainloop()
 
