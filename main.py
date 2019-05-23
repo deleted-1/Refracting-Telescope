d5ee3b9928dfbad822fcc5f0f1009be6546ec02a
Dominic Humber <dhumber.buisness@gmail.com>
2019-05-23 00:55:56 -0400

    Update

--- main.py
+++ main.py
@@ -1,91 +1,68 @@
 import pygame as pg
-import math
-from createRay import Rays
+from createRay import *
 
+pg.init()
 pg.font.init()
 
 def draw_circles(background,coordinates):
     for i in range(len(coordinates)):
-        pg.draw.circle(background,(0,0,0),coordinates[i],5) 
+        pg.draw.circle(background,(0,0,0),coordinates[i],5)
 
-def main(height_object=70,distance_object=140,focal_length1=50,focal_length2=20,distance_ocular=100,image=None,distance_objective=450):
+def main(height_object=70,distance_object=120,focal_length1=50,focal_length2=20,distance_ocular=150,distance_objective=450):
     screen = pg.display.set_mode((1000,900), pg.RESIZABLE)
-    if image == None: image = "tree"
     pg.display.set_caption("Refracting Telescope")
-    height_objective=300
-    height_ocular=150
-    distance_ocular+=450
-    clock = pg.time.Clock()
-    object_ = pg.image.load(image+".png")
+
+    font = pg.font.Font('freesansbold.ttf',15)
+    object_ = pg.image.load("tree.png")
     object_ = pg.transform.scale(object_, [height_object,height_object])
     object_ = object_.convert()  
-    
-    
+    distance_ocular+=450
+
+    rays = []
     center_curvature1 = [distance_objective+12, 450]
     center_curvature2 = [distance_ocular+12, 450]
     focal_objective1 = [distance_objective-focal_length1,450]
     focal_objective2 = [distance_objective+25+focal_length1,450] 
     focal_ocular1 = [distance_ocular-focal_length2,450] 
-    focal_ocular2 = [distance_ocular+25+focal_length2,450]  
+    focal_ocular2 = [distance_ocular+25+focal_length2,450] 
+    real_image_drawn = False
+    virtual_image_drawn = False
 
-    final_destination = [focal_ocular2[0],focal_ocular2[1]]
-
-    slope = [(focal_objective2[0]-center_curvature1[0])/60,(450-450-height_object)/60]
-    slope1 = [(center_curvature1[0]-distance_object-int(height_object/2))/60 , (450-450-height_object)/60]
-    slope2 = [(center_curvature1[0]-distance_object+int(height_object/2))/60,(center_curvature1[1]-(450-height_object))/60]
-
-    
-
-    x = focal_objective2[0]
-    y = 450
-    x1 = focal_objective1[0]
-    y1 = 450
-    x2 = center_curvature1[0]
-    y2 = 450
-
-    while x < center_curvature2[0]-slope[0] :
-        if  x < center_curvature2[0]-slope[0]:
-            x += slope[0]
-            y -= slope[1]
-
-
-    while x1 < center_curvature1[0]-slope1[0]:
-        x1 += slope1[0]
-        y1 -= slope1[1]
-
-    while x2 < center_curvature2[0]-slope2[0]:
-        x2 += slope2[0]
-        y2 += slope2[1]
-
-    second_x = x
-    second_x2 = x2
-    second_y = y
-    second_y2 = y2
-
-    while second_y <= second_y2 and second_x <= second_x2:
-        second_x -= slope[0]
-        second_y -= slope[1]
-        second_y2 -= slope2[1]
-        second_x2 -= slope2[0]
-
-    ray1 = Rays( [distance_object+int(height_object/2),450-height_object] , [center_curvature1[0],450-height_object] , [x,y] , final_destination)
-    ray2 = Rays ([distance_object+int(height_object/2),450-height_object],[x1,y1],[center_curvature2[0],y1],final_destination)
-    ray3 = Rays ([distance_object+int(height_object/2),450-height_object],[x2,y2],final_destination)
+    distance_image1 = (1/focal_length1 - 1/distance_object)**-1 
+    height_image1 = (distance_image1*height_object/distance_object)
+    distance_image2 = (1/focal_length2 - 1/distance_image1)**-1
+    height_image2 = (distance_image2*height_image1/(distance_ocular-distance_image1))
+    magnefication1 = -distance_image2/distance_image1
+    magnefication2 = -distance_image1/distance_object
 
     background = pg.Surface(screen.get_size())
     background = background.convert()
     background.fill((210 , 180 , 140))   
-    
-    #BigSystemFont = pg.font.SysFont("Arial", 30)
-    #SmallSystemFont = pg.font.SysFont("Arial", 15)
-    #label1 = BigSystemFont.render("F", 1, (255, 255, 0))
-    #label2 = SmallSystemFont.render("O", 1, (255, 255, 0))
-    pg.draw.line(background,(0,0,0),[100,450],[900, 450],3)# Boundary/Plane
-    is_real_image = False
+    clock = pg.time.Clock()
 
-    objective = pg.draw.ellipse(background,(135, 206, 235),[distance_objective,450-int(height_objective*.5),25,height_objective],0)
-    ocular = pg.draw.ellipse(background,(135, 206, 235),[distance_ocular,450-int(height_ocular*.5),25,height_ocular],0)
+    pg.draw.line(background,(0,0,0),[100,450],[900, 450],3)# Boundary/Plane
+
+    objective = pg.draw.ellipse(background,(135, 206, 235),[distance_objective,450-int(500*.5),25,400],0)
+    ocular = pg.draw.ellipse(background,(135, 206, 235),[distance_ocular,450-int(350*.5),25,300],0)
     draw_circles(background,[center_curvature1,center_curvature2,focal_objective1,focal_objective2,focal_ocular1,focal_ocular2])
+
+    slope_ray1 = [((focal_objective2[0])-(center_curvature1[0]))/60, ((focal_objective2[1])-(450-height_object))/60]
+    x_ray1 = center_curvature1[0]
+    y_ray1 = 450-height_object
+    while x_ray1 <= center_curvature2[0]:
+        x_ray1 += slope_ray1[0]
+        y_ray1 += slope_ray1[1]
+
+    slope_ray3 = [((center_curvature1[0])-(450-distance_object-int(height_object/4)))/60, ((center_curvature1[1])-(450-height_object))/60]
+    x_ray3 = 450-distance_object-int(height_object/4)
+    y_ray3 = 450-height_object
+    while x_ray3 <= center_curvature2[0]:
+        x_ray3 += slope_ray3[0]
+        y_ray3 += slope_ray3[1]
+
+    rays.append( Rays( [450-distance_object-int(height_object/4),450-height_object] , [center_curvature1[0],450-height_object] , [x_ray1,y_ray1] , [distance_image2,height_image2]) )
+    rays.append( Rays( [450-distance_object-int(height_object/4),450-height_object] , [center_curvature1[0],450+height_image1] , [center_curvature2[0],450+height_image1] , [distance_image2,height_image2]) )
+    rays.append( Rays( [450-distance_object-int(height_object/4),450-height_object] , [x_ray3,y_ray3] , [distance_image2,height_image2]))
 
     while True:
         clock.tick(60)     
@@ -93,28 +70,31 @@ def main(height_object=70,distance_object=140,focal_length1=50,focal_length2=20,
         for event in pg.event.get():
             if event.type == pg.QUIT:   break
 
-        ray1.run(background)
-        ray2.run(background)
-        ray3.run(background)
-        
+        for ray in rays: ray.run(background)
 
-        if ray1.x >= float(final_destination[0])-slope[0] and ray2.x >= float(final_destination[0])-slope1[0] and ray3.x >= float(final_destination[0])-slope2[0]: break
-
+        y1 = rays[0].y
+        y2 = rays[1].y
+        y3 = rays[2].y
         screen.blit(background, (0,0))
-        screen.blit(object_, (450-distance_object, 450-height_object))
-        if ray1.y <= ray2.y and is_real_image == False and ray1.y>450 and ray1.x <= ray2.x: 
-            intersection = [int(ray1.x),int(ray2.y)]
-            virutal_image = pg.image.load(image+".png")
-            virutal_image = pg.transform.rotate(virutal_image,180)
-            virutal_image = pg.transform.scale(virutal_image, [intersection[1]-450,intersection[1]-450])
-            intersection_sentence = 'The real image is at a height of {}cm and {}cm away from the ocular lens.'.format(intersection[1]-450,distance_ocular-intersection[0])
-            print(intersection_sentence)
-            virutal_image.convert()
-            screen.blit(virutal_image, [intersection[0]+height_object,intersection[1]-height_object])
-            is_real_image = True
-        elif is_real_image:  screen.blit(virutal_image, [intersection[0]+(intersection[1]-450)/2,intersection[1]-(intersection[1]-450)])
-        #screen.blit(label1, (350, 460))
-        #screen.blit(label2, (360, 475))
-        pg.display.flip()
+        screen.blit(object_, (450-distance_object-int(height_object/2), 450-height_object))
+        if real_image_drawn == False and y1 >= height_image1+450 and y2 >= height_image1+450 and y3 >= height_image1+450:
+            real_image_drawn = True
+            real_image = pg.image.load("tree.png")
+            real_image = pg.transform.rotate(real_image, 180)
+            real_image = pg.transform.scale(real_image, [int(height_image1),int(height_image1)])
+            real_image.convert()
+            intersection = 'The size of the real image is {:.2f}cm and is {:.2f}cm away from the ocular lens.'.format(height_image1,distance_ocular-distance_image1-450)
+            magnefication1 = 'The magnefication of the real image is {:.2f}.'.format(magnefication1)
+            intersection = font.render(intersection, True, [0,0,0])
+            magnefication1 = font.render(magnefication1, True, [0,0,0])
+            screen.blit(magnefication1,[100,130])
+            screen.blit(intersection,[100,100])
+            screen.blit(real_image,[int(450+distance_image1+height_image1/8),int(450)])
+        elif real_image_drawn == True: 
+            screen.blit(real_image,[int(450+distance_image1+height_image1/8),int(450)])
+            screen.blit(intersection,[100,100])
+            screen.blit(magnefication1,[100,130])
 
+        pg.display.flip()
+#main()
 pg.quit()
--- window.py
+++ window.py
@@ -1,17 +1,17 @@
 from tkinter import *
 from main import main
 
-def set_parameters(image=None):
+def set_parameters():
 
     root = Tk()
     root.title("Telescope Simulation")
-    root.geometry("500x225+0+0")
+    root.geometry("500x320+0+0")
 
     Label(root, text="Enter the height of the object(cm)", font=("time new roman",10)).place(x=20,y=20)
-    Label(root, text="Enter the object distance from the objective lens(cm)", font=("time new roman",10)).place(x=20,y=45)
-    Label(root, text="Enter the focal point of the objective lens(cm)", font=("time new roman",10)).place(x=20,y=70)
-    Label(root, text="Enter the focal point of the ocular lens(cm)", font=("time new roman",10)).place(x=20,y=95)
-    Label(root, text="Enter the distance between the two lens(cm)",font=("time new roman",10)).place(x=20,y=120)
+    Label(root, text="Enter the object distance from the objective lens(cm)", font=("time new roman",10)).place(x=20,y=70)
+    Label(root, text="Enter the focal point of the objective lens(cm)", font=("time new roman",10)).place(x=20,y=120)
+    Label(root, text="Enter the focal point of the ocular lens(cm)", font=("time new roman",10)).place(x=20,y=170)
+    Label(root, text="Enter the distance between the two lens(cm)",font=("time new roman",10)).place(x=20,y=220)
 
     height_object=Entry(root)
     distance_object=Entry(root)
@@ -19,36 +19,23 @@ def set_parameters(image=None):
     focal_ocular=Entry(root)
     len_distance=Entry(root) 
 
-    def images():
-        list_image = Tk()
-        list_image.title("Images")
-        list_image.geometry("500x225+300+0")
-        def tree(): 
-            list_image.destroy()
-            root.destroy()
-            set_parameters("tree")
-            
-        def tomato():
-            list_image.destroy()
-            root.destroy()
-            set_parameters("tomato")
-                           
-        Button(list_image, text="tree", width=5, command=tree).place(x=20,y=20)
-        Button(list_image, text="tomato", width=5, command=tomato).place(x=20,y=45)
-
     def show_diagram():
-        try:    main(int(height_object.get()),int(distance_object.get()),int(focal_objective.get()),int(focal_ocular.get()),int(len_distance.get()),image)
-        except ValueError:  Label(root, text="*Please use integer values.", font=("time new roman",10), fg='red').place(x=225,y=145)        
+        try:    
+            if int(distance_object.get()) <= int(focal_objective.get())*2:    Label(root, text="Has to be twice focal objective!", font=("time new roman",10), fg='red').place(x=300,y=45)
+            elif float((1/float(focal_objective.get())- 1/float(distance_object.get()))**-1)  < float(focal_ocular.get()):   Label(root, text="Please specify other inputs because the real image is beyond the focal of the ocular!", font=("time new roman",10), fg='red').place(x=50,y=145)
+            elif int(distance_object.get()) > 200:  Label(root,text="Please choose an integer smaller or equal to 200 for object distance.", font=("time new roman",10), fg='red').place(x=350,y=45)
+            elif int(len_distance.get())-int(focal_ocular.get()) <= focal_objective | int(focal_ocular.get()) <= 0: Label(root, text="Please enter a larger focal ocular.", font=("time new roman",10), fg='red').place(x=325,y=95) 
+            else:   main(int(height_object.get()),int(distance_object.get()),int(focal_objective.get()),int(focal_ocular.get()),int(len_distance.get()))
+        except ValueError:  Label(root, text="*Please use integer values.", font=("time new roman",10), fg='red').place(x=20,y=245)        
     
     height_object.place(x=225,y=20)
-    distance_object.place(x=340, y=45)
-    focal_objective.place(x=300,y=70)
-    focal_ocular.place(x=275,y=95)
-    len_distance.place(x=295,y=120)
+    distance_object.place(x=340, y=70)
+    focal_objective.place(x=300,y=120)
+    focal_ocular.place(x=275,y=170)
+    len_distance.place(x=295,y=220)
  
-    Button(root, text='Quit', width = 5,command=root.quit).place(x=20,y=170)
-    Button(root, text='Submit',width=5, command=show_diagram).place(x=100,y=170)
-    Button(root, text='Images', width=5, command=images).place(x=180,y=170)
+    Button(root, text='Quit', width = 5,command=root.quit).place(x=20,y=270)
+    Button(root, text='Submit',width=5, command=show_diagram).place(x=100,y=270)
 
     mainloop()
 
